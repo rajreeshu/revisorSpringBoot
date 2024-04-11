63564ec6dc31226b2e402622b9eb196b17ea2472
ReeshU Raj <reeshu54raj@gmail.com>
2024-04-11 12:18:39 +0530

    minor change in randomQuestion sql

--- src/main/java/com/revise/leetcode/leetcodeRevision/repository/QuestionRepository.java
+++ src/main/java/com/revise/leetcode/leetcodeRevision/repository/QuestionRepository.java
@@ -14,7 +14,7 @@ import com.revise.leetcode.leetcodeRevision.entity.User;
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 	// Example using JPQL (adjust based on your database)
-	@Query(value = "SELECT * FROM question WHERE label = ?1 AND user_id=?3 AND category_id=?4  ORDER BY RAND() LIMIT ?2", nativeQuery = true)
+	@Query(value = "SELECT * FROM question WHERE label = ?1 AND user_id=?3 AND category_id=?4  ORDER BY RAND()*RAND() LIMIT ?2", nativeQuery = true)
     List<Question> findRandomQuestionsByLabelAndCategory(String label, int limit, long userId, long category);
 	
 	int countByUserAndLabelAndCategory(User user, Difficulty difficulty, Category category);
